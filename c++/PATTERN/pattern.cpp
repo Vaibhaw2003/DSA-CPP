28c9340519fecdb3c44c89d4982faed5714783c2
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-02 15:35:00 +0530

    pattern

---
+++ c++/PATTERN/pattern.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int i, j;
+for ( i = 1; i <=5; i++) //j is here the number of line
+{
+    for ( j = 1; j <=5; j++) //j is here the number of number
+{
+
+    cout<<i;
+}
+cout<<endl;
+}
+}
+
+
+
+
+
+
+
+// {
+
+//     int i, j;
+// for ( i = 1; i <=5; i++) //j is here the number of line
+// {
+//     for ( j = 1; j <=5; j++) //j is here the number of number
+// {
+
+//     cout<<j;
+// }
+// cout<<endl;
+// }
+// }
+
+
+
+
+    // PATTERN PRINTING OF 5*3---------
+
+// for (int  j = 1; j <=3; j++) // for number of row
+// j
+//     for ( int i = 1; i <=5; i++)   // for number of colum
+// {
+//     cout<<"*"<<" ";
+// }
+// cout<<endl;
+// }
+// }
