3da04feb21458ac133b8838c5cda2a47fb0bc159
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-03 01:53:31 +0530

    new

--- c++/PATTERN/pattern.cpp
+++ c++/PATTERN/pattern.cpp
@@ -2,19 +2,157 @@
 using namespace std;
 
 int main()
-{
 
-    int i, j;
-for ( i = 1; i <=5; i++) //j is here the number of line
 {
-    for ( j = 1; j <=5; j++) //j is here the number of number
-{
+// int i,j;
+// for ( i = 1; i <=5; i++)
+// {
+//     for ( j = 1; j<= 5-i-1; i++)
+//     {
+//         cout<<j<<" ";
+//     }
+//     cout<<endl;
+// }
 
-    cout<<i;
-}
-cout<<endl;
-}
-}
+
+// }
+
+
+// TO PRINT  
+    // *****
+    // ****
+    // ***
+    // **
+    // *    THISE TYPE OF PATTER CODE BELOWE------
+// {
+//     int i,j;
+
+//     for ( i = 5; i >=1; i--)
+//     {
+       
+//         for ( j = 1; j <= i; j++)
+//         {
+//               cout<<'*'<<" ";
+//         }
+//             cout<<endl;
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TO PRINT THE 
+    // a
+    // bb
+    // ccc
+    // dddd
+    // eeeee patter code belowe-------
+
+// {  
+// int row, col;
+// for ( row = 1; row <=10; row++)
+// {
+//    char name = 'a'+ row-1;
+
+//     for ( col = 1; col<=row; col++)
+//     {
+//         cout<<name<<" ";
+//     }
+//     cout<<endl;
+// }
+// }
+
+
+
+
+ // 05 TO PRINT THE 
+    //   1
+    //   22
+    //   333
+    //   4444
+    //   55555    THIS PATTERN-------BELOW
+// {
+//      int row, col;
+//    for ( row = 1; row <=5; row++)
+//    {
+//        for ( col = 1; col <= row; col++)      
+//          {
+//            cout<<row<<" ";
+//        }        cout<<endl;
+//             } 
+//     }
+
+
+// 06 TO PRINT THISE PATTER
+    //   1
+    //   21
+    //   321
+    //   4321
+    //   54321     PTINT THISE PATTER BELLOW-------
+
+// {
+//   int row, col;
+
+//     for ( row = 1; row <=5; row++)
+//      {
+//          for ( col = row; col >= 1; col--)
+//          {
+//              cout<<col<<" ";
+//          }
+//          cout<<endl;
+        
+//      } 
+//  }
+
+
+
+
+
+
+// TO PRINT TRANGULAR * ------
+
+// {
+//    int n=4;
+//    for(int i=0; i<n; i++){
+//     // for starting space in pyramid
+//     for(int j=0; j<n-i-1; j++){
+//         cout<<" ";
+//     }
+//     //for stars 
+//     for(int k=0; k<2*i+1; k++){
+//         cout<<"*";
+//     }
+//     cout<<endl;
+//    }
+// }
+
+
+  // TO PRINT SAME NUMBER IN ALL LINE ----
+// {
+
+//     int i, j;
+// for ( i = 1; i <=5; i++) //j is here the number of line
+// {
+//     for ( j = 1; j <=5; j++) //j is here the number of number
+// {
+
+//     cout<<i;
+// }
+// cout<<endl;
+// }
+// }
 
 
 
@@ -41,8 +179,7 @@ cout<<endl;
 
     // PATTERN PRINTING OF 5*3---------
 
-// for (int  j = 1; j <=3; j++) // for number of row
-// j
+// for (int  j = 1; j <=3; j++){   // for number of rowj
 //     for ( int i = 1; i <=5; i++)   // for number of colum
 // {
 //     cout<<"*"<<" ";
--- c++/PATTERN/tempCodeRunnerFile.cpp
+++ c++/PATTERN/tempCodeRunnerFile.cpp
@@ -1,1 +1,11 @@
-cout<<j<<endl;
+      int row, col;
+    for ( row = 1; row <=5; row++)
+    {
+        for ( col = 1; col <= row; col++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+        
+    } 
+}
