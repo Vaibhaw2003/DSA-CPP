a2044ab8102da87e13a87a79384b4ff951a9752d
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-03 16:56:38 +0530

    DONE

---
+++ c++/PATTERN/hardpattern.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // 00---TO PRINT "ABCD" IN PATTERN CODE BELOWE----
+
+// int i, j; // i= row ,j= col, k= *
+// for ( i = 0; i < 6; i++)  //for row ke liye
+// {
+//     for ( j = 0; j < 6-i; j++)  // for spacing ke liye
+//     {
+//        cout<<" ";
+//     }
+//     for (char name = 'a'; name <'a' +(i-1); name++) // for printing *
+//     {
+//        cout<<name;
+//     }
+//     cout<<endl; 
+// }
+
+    //  01--  TO PRINT 1
+            //    12
+            //   123
+            //  1234  THISE Ptter code below--------
+
+// int i, j,k; // i= row ,j= col, k= *
+// for ( i = 0; i < 6; i++)  //for row ke liye
+// {
+//     for ( j = 0; j < 6-i; j++)  // for spacing ke liye
+//     {
+//        cout<<" ";
+//     }
+//     for ( j = 0; j <i; j++) // for printing *
+//     {
+//        cout<<j;
+//     }
+//     cout<<endl; 
+// }
+
+
+
+    // 02--- TO PRINT      1
+            //     22
+            //    333
+            //   4444  THISE PATTER CODE BELOW----
+// int i, j,k; // i= row ,j= col, k= *
+// for ( i = 0; i < 5; i++)  //for row ke liye
+// {
+//     for ( j = 0; j < 5-i; j++)  // for spacing ke liye
+//     {
+//        cout<<" ";
+//     }
+//     for ( k = 0; k < i; k++) // for printing *
+//     {
+//        cout<<i;
+//     }
+//     cout<<endl;
+// }
+
+
+
+    // 03----  TO PRINT PYRAMID OF * ---------
+
+// int i, j,k; // i= row ,j= col, k= *
+// for ( i = 0; i < 11; i++)  //for row ke liye
+// {
+//     for ( j = 0; j < 11-i; j++)  // for spacing ke liye
+//     {
+//        cout<<" ";
+//     }
+//     for ( k = 0; k < i; k++) // for printing *
+//     {
+//        cout<<"*";
+//     }
+//     cout<<endl;
+    
+// }
+
+
+
+     // 04----- TO PRINT * BY 5 -------
+    // int i, j;
+
+    // for ( i = 0; i < 6; i++)
+    // {
+    //    for ( j = 0; j < i-1; j++)
+    //    {
+    //        cout<<" *";
+    //    }
+    //    cout<<endl;
+       
+    // }
+    
+
+}
