67c9a2f2ae0839538ec2f65c45eba04779ea77df
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-10 23:40:18 +0530

    ADD

--- c++/Type Casting/s.cpp
+++ c++/Type Casting/s.cpp
@@ -1,10 +1,77 @@
 // Your First C++ Program
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
+// int main() {
 
-    int chach =10, b=40, c = 60;
-    cout<<b+c;
-}
+//     int chach =10, b=40, c = 60;
+//     cout<<b+c;
+// }
+
+
+
+   //------ADITION OF TWO NUMBER------
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a,b;
+//     cout<<"enter number a: ";
+//     cin>>a;
+//     cout<<"enter number b: ";
+//     cin>>b;
+//     int sum = a+b;
+//     cout<<sum;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char let;
+//      cout<<"enter the alphabet: ";
+//      cin>> let;
+
+//      if (char a = let)
+//      {
+//        cout<<"vowel";
+//      }
+//      else if (char e = let)
+//      {
+//         cout<<"vowel";
+//      }
+//      else if (char i = let)
+//      {
+//         cout<<"vowel";
+//      }
+//     else{
+//         cout<<"consonent";
+//      }     
+
+
+
+
+// -----TO CHECK VOWEL OR CONSONENT USING OR (||) OPERATOR-----
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     char ch;
+
+//     cout << "Enter a character: ";
+//     cin >> ch;
+
+//     // check vowel using OR (||)
+//     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+//         ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+//         cout << ch << " is a vowel." << endl;
+//     } else {
+//         cout << ch << " is a consonant." << endl;
+//     }
+
+//     return 0;
+// }
