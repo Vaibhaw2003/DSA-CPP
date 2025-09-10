7f99ca93cd64992eb85c12b7b14213e46d1e789f
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-11 01:03:02 +0530

    while

---
+++ c++/WHILE_LOOPS/whileloop.cpp
@@ -0,0 +1,29 @@
+
+   // TO print numbers from 1 to 10 using while loop------
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     while(i<=10){
+//         cout<<i<<endl;
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+
+    // TO print numbers from 10 to 1 using while loop------
+
+    // #include<iostream>
+    // using namespace std;
+
+    // int main(){
+    //     int i=10;
+    //     while(i>=1){
+    //         cout<<i<<endl;
+    //         i--;
+    //     }
+    //     return 0;
+    // }
