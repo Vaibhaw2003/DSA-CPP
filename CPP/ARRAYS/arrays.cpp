e92386437dd11dc0932f6e536be147f53f43ac39
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-20 23:34:53 +0530

    arrays

---
+++ CPP/ARRAYS/arrays.cpp
@@ -0,0 +1,47 @@
+// CPP program to demonstrate array
+// An array is a collection of items stored at contiguous memory locations.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     int i;
+//     int name[5]={1,2,3,4,5};
+
+//     for (i=0; i<5; i++)
+//     {
+//         cout<<name[i]<<endl;
+//     }
+// }
+
+
+// CPP program to demonstrate array initialization-------
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i;
+//     int a [3]={0};
+//     cout<<a[0]<<endl;
+// }
+
+
+// CPP program to demonstrate array input and output-------
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i;
+//     int a[4]={};
+//     cin>>a[0,1,2,3];
+//     cout<<a[0,1,2,3]<<endl;
+// }
+
+
+
+// CPP program to demonstrate array access---------
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int name []={1,2,3,4,5};
+//     cout<<name[2]<<endl;
+// }
