1a76f6c13076abdc5bf4d738fc960385c8f7c116
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-24 00:34:02 +0530

    odd

--- CPP/IF ELSE/pos_neg.cpp
+++ CPP/IF ELSE/pos_neg.cpp
@@ -1,22 +1,42 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num;
+
+//     cout << " entera num to check pos neg or 0: ";
+//     cin >> num;
+//     cout<< num<< endl;
+
+//     if (num>0)
+//     {
+//         cout<<"Positive";
+//     }
+//     else if (num == 0){
+//         cout<< "Equal to 0";
+//     }
+//     else
+//     cout<<"Negative";
+
+// }
+
+
+//printing the number chech even or odd----
+
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    int num;
+int main(){
+    int a;
+    cout<<"enter the number : ";
+    cin>>a;
 
-    cout << " entera num to check pos neg or 0: ";
-    cin >> num;
-    cout<< num<< endl;
-
-    if (num>0)
+    if (a%2==0)
     {
-        cout<<"Positive";
-    }
-    else if (num == 0){
-        cout<< "Equal to 0";
+       cout<<"even number:";
     }
     else
-    cout<<"Negative";
+    cout<<"oddnumber";
 
 }
