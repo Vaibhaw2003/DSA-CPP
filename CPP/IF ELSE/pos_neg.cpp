bad67cef5e5ffb47c0ad6b177c00bba6bd63b37c
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-07 03:00:42 +0530

    array sum

--- CPP/IF ELSE/pos_neg.cpp
+++ CPP/IF ELSE/pos_neg.cpp
@@ -24,19 +24,20 @@
 
 //printing the number chech even or odd----
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
-    int a;
-    cout<<"enter the number : ";
-    cin>>a;
+// int main(){
+//     int a;
+//     cout<<"enter the number : ";
+//     cin>>a;
 
-    if (a%2==0)
-    {
-       cout<<"even number:";
-    }
-    else
-    cout<<"oddnumber";
+//     if (a%2==0)
+//     {
+//        cout<<"even number:";
+//     }
+//     else
+//     cout<<"oddnumber";
 
-}
+// }
+
--- CPP/LOOPS/n_times.cpp
+++ CPP/LOOPS/n_times.cpp
@@ -1,21 +1,63 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//  int num;
+//     cout<< "Enter a num:";
+//     cin>>num;
+    
+//     for(int i= 1; i<=num ; i++){
+//      if (i%2==0)
+//      {
+//         cout<< i <<endl;
+//      }
+//      else{}
+//     }
+// }
+        
+
+
+//------to print the sum of array------
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int a,i;
+//    cout<<"enter the number : ";
+//    cin>>a;
+
+//    for ( i = 0; i <a; i++)
+//    {
+//       cout<<i<<" ";
+//    }
+//    cout<<endl;
+// }
+
+
+
+// ---------To print the sum of all the number of user-------
+
+#include<iostream>
 using namespace std;
 
-int main()
-{
- int num;
-    cout<< "Enter a num:";
-    cin>>num;
-    
-    for(int i= 1; i<=num ; i++){
-     if (i%2==0)
-     {
-        cout<< i <<endl;
-     }
-     else{}
-    }
+int main(){
+   int a ,i,pre, cur;
+   
+   cout<<"enter the number you want to sum : ";
+   cin>>a;
+
+   for ( i = 0; i < a; i++)
+   {
+      pre = 0;
+      cur = pre + i;
+      int sum = cur+ i;
+      cout<<sum<<" ";
+      
+   }cout<<endl;
+   
 }
-        
+
 
 
 
