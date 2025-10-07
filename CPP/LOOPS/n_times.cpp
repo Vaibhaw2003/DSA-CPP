ff5a13a005939e4a509df72d25b47daf3920e7f0
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-07 11:27:38 +0530

    patter_02

--- CPP/LOOPS/n_times.cpp
+++ CPP/LOOPS/n_times.cpp
@@ -38,26 +38,61 @@
 
 // ---------To print the sum of all the number of user-------
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
-   int a ,i,pre, cur;
+// int main(){
+//    int a ,i,pre, cur;
    
-   cout<<"enter the number you want to sum : ";
-   cin>>a;
+//    cout<<"enter the number you want to sum : ";
+//    cin>>a;
 
-   for ( i = 0; i < a; i++)
-   {
-      pre = 0;
-      cur = pre + i;
-      int sum = cur+ i;
-      cout<<sum<<" ";
+//    for ( i = 0; i < a; i++)
+//    {
+//       pre = 0;
+//       cur = pre + i;
+//       int sum = cur+ i;
+//       cout<<sum<<" ";
       
-   }cout<<endl;
+//    }cout<<endl;
    
-}
+// }
 
+
+//  -------TO PRINT THE TABBLE OF N NUMBER-------
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//    int n, i;
+//    cout<<" enter the n number : ";
+//    cin>>n;
+
+//    for ( i = 1; i <= 10; i++)
+//    {
+//       cout<<n*i<< " ";
+//    }
+   
+// }
+
+
+// -------TO PRINT THE PATTER-------
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int r,c,i;
+
+//    for ( r = 1; r<=5; r++)
+//    {
+      
+//       for ( c = 1; c <= r; c++)
+//       {
+//         cout<<c<<" ";
+//       }
+//       cout<<endl;    
+//    }
+// }
 
 
 
