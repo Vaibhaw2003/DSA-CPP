7cb3bc9f4a54ab11019d0d4de8f2ad5166dc9909
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-09 21:30:22 +0530

    marks

--- CPP/IF ELSE/if_else.cpp
+++ CPP/IF ELSE/if_else.cpp
@@ -1,33 +1,57 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     // int salary;
+
+//     // cout << " enter your offer sallry: ";
+//     // cin >> salary;
+//     // cout<< salary<< endl;
+
+//     // if (salary>=1000)
+//     // {
+//     //     cout<<" i accept you";
+//     // }
+//     // else {
+//     //     cout<< "not accept ";
+//     // }
+
+//     float marks;
+//     cout << "Enter yourr 12th boards marks: " << endl;
+//     cin >> marks;
+//     cout << "YOUR MARKS : " << marks << endl  ;
+
+//     if (marks <= 33)
+//     {
+//         cout << "You Have been failed" << endl  << marks;
+//     }
+//     else
+//     {
+//         cout << "You Have been promoted" << endl << marks;
+//     }
+// }
+
+
+// -------to check fail or pass -----
+
+#include<iostream>
 using namespace std;
-
-int main()
-{
-    // int salary;
-
-    // cout << " enter your offer sallry: ";
-    // cin >> salary;
-    // cout<< salary<< endl;
-
-    // if (salary>=1000)
-    // {
-    //     cout<<" i accept you";
-    // }
-    // else {
-    //     cout<< "not accept ";
-    // }
-
-    float marks;
-    cout << "Enter yourr 12th boards marks: " << endl;
-    cin >> marks;
-    cout << "YOUR MARKS : " << marks << endl  ;
-
-    if (marks <= 33)
+ int main(){
+    int mark;
+    cout<<"enter the marks : ";
+    cin>>mark;
+    if (mark>=90)
     {
-        cout << "You Have been failed" << endl  << marks;
-    }
-    else
+        /* code */cout<<"A+";
+    }else if (mark>=60)
     {
-        cout << "You Have been promoted" << endl << marks;
+        /* code */cout<<"B";
+    }else if (mark>=40)
+    {
+        /* code */cout<<"c";
+    }else{
+        cout<<" fail";
     }
-}
+    
+ }
