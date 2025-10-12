5361197da0072025eea3296d5b18d5e4c788b19b
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-13 00:11:27 +0530

    array

---
+++ CPP/ARRAYS/array_02.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+
+    int array[5] ={1,2,3,4,5};
+    cout<<array;
+
+    for (int  i = 0; i <5; i++)
+    {
+        /* code */cout<<i;
+    }
+    
+}
