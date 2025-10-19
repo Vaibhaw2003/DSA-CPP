4cdbdaa30bdc3bc7f79099778ddccc33384b5151
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-10-19 15:25:00 +0530

    practice

--- CPP/ARRAYS/array_02.cpp
+++ CPP/ARRAYS/array_02.cpp
@@ -1,14 +1,51 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+
+//     int array[5] ={1,2,3,4,5};
+//     cout<<array;
+
+//     for (int  i = 0; i <5; i++)
+//     {
+//         /* code */cout<<i;
+//     }
+    
+// }
+
+
+
+// ------To print the sum of two number-----
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+  //  int a,b;
+    //cout<<"enter the number: ";
+    //cin>>a;
+    //cout<<"enter the number b : ";
+    //cin>>b;
+    //int sum = a +b;
+    //cout<<sum;
+//}
+
+//------TO PRINT THE PATTERN OF * IN TRANGULAR FORM --------
+
 #include<iostream>
 using namespace std;
-int main() {
-    int n;
 
-    int array[5] ={1,2,3,4,5};
-    cout<<array;
+int main(){
+    int i,j,n;
+    cout<<"enter the number: ";
+    cin>>n;
 
-    for (int  i = 0; i <5; i++)
+    for ( i = 0; i < 5; i++)
     {
-        /* code */cout<<i;
+        for ( j = 0; j<n; j++)
+        {
+            cout<<j<<endl;
+        }
+        cout<<endl;  
     }
-    
+
 }
