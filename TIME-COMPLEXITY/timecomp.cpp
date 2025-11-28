87aa7aa4eccb83f2d094ae7ad1481bd1c34f3a74
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-11-28 21:42:04 +0530

    binary search

--- TIME-COMPLEXITY/timecomp.cpp
+++ TIME-COMPLEXITY/timecomp.cpp
@@ -17,7 +17,7 @@
 // }
 
 // TO FIND THE ELEMENT IN THE ARRAY-------
-
+/*  
 #include <iostream>
 using namespace std;
 
@@ -38,6 +38,46 @@ int main() {
         if(arr[i] == key){
             cout << "Element found at index " << i;
             return 0;
+        }
+    }
+
+    cout << "Element not found";
+    return 0;
+}
+*/
+
+
+// binary search------
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter sorted elements: ";
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    cout << "Enter element to find: ";
+    cin >> key;
+
+    int low = 0, high = n - 1;
+
+    while(low <= high){
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == key){
+            cout << "Element found at index " << mid;
+            return 0;
+        }
+        else if(arr[mid] < key){
+            low = mid + 1;
+        } 
+        else {
+            high = mid - 1;
         }
     }
 
