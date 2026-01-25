3469540caeea08ec6d786fcf30be8a0279c4a77d
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-01-26 01:54:08 +0530

    Array Input

--- SORTING/selection.cpp
+++ SORTING/selection.cpp
@@ -62,25 +62,65 @@ int main(){
 
 //binary search after selection sort--------
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i,j;
+//     int arr[6] = {29,10,14,37,13,25};
+//     for ( i = 0; i < 5; i++)
+//     {
+//         int index = i;
+//         for ( j = i+ 1; j <6; j++)
+//         {
+//             if (arr[j]< arr[index])
+//             {
+//                 index = j;
+//             }   
+//         }   
+//         swap(arr[index], arr[i]);
+//     }
+//     for ( i = 0; i < 6; i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     } 
+// }
+
+// TO SORT N NUMBER OF ARRAYS -------
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[5];
+
+//     cout << "Enter 5 numbers:\n";
+//     for(int i = 0; i < 5; i++) {
+//         cin >> arr[i];
+//     }
+
+//     cout << "You entered:\n";
+//     for(int i = 0; i < 5; i++) {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// TO TAKE SIZE AND ELEMENT OF THE ARRAY -------
+
+#include<iostream>
 using namespace std;
 int main(){
-    int i,j;
-    int arr[6] = {29,10,14,37,13,25};
-    for ( i = 0; i < 5; i++)
+    int i,j,n;
+    cout<<"enter the size of arrays: ";
+    cin>>n;
+     int arr[n];
+    cout<<"enter the element of arrays :";
+    for (int i = 0; i <=n; i++)
     {
-        int index = i;
-        for ( j = i+ 1; j <6; j++)
-        {
-            if (arr[j]< arr[index])
-            {
-                index = j;
-            }   
-        }   
-        swap(arr[index], arr[i]);
+        cin>>arr[i];
     }
-    for ( i = 0; i < 6; i++)
+    cout<<"you enterd the number is :";
+    for ( i = 0; i <=n; i++)
     {
         cout<<arr[i]<<" ";
-    } 
+    }
 }
