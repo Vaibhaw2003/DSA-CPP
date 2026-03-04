f201831217fcd05c25b74b4a241e4ca356ae1992
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-04 12:08:55 +0530

    RECURSION

---
+++ RECURSION/Demo.cpp
@@ -0,0 +1,53 @@
+//------------- RECURSION -----------
+
+// #include<iostream>
+// using namespace std;
+
+// void printnum (int n ){
+//     if(n==0){
+//     return;
+//     }
+
+//     cout<<n<<" ";
+//     printnum (  n*(n-1) );
+// }
+
+// int main(){
+//     printnum(4);
+//     return 0;
+
+// }
+
+//--------- factorial--------
+
+// #include<iostream>
+// using namespace std;
+
+// int factorial(int n){
+//     if(n == 0 || n == 1)
+//         return 1;
+
+//     return n * factorial(n - 1);
+    
+// }
+// int main(){
+//     //factorial(4);
+//     cout<<factorial(4);
+// }
+
+//--------- part -02 -------
+
+// #include<iostream>
+// using namespace std;
+// int fact (int n ){
+//     if(n==0 || n==1)
+//     return 1;
+
+//     return n * fact(n-1);
+// }
+// int main(){
+//     cout<<fact(6);
+// }
+
+
+//---------- N-QUEENS -----------
